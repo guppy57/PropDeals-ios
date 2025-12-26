b45574eb66f6df67c160e29415cb25f543099648
Armaan Gupta <armaan@guppy.im>
2025-12-26 10:44:09 -0600

    init:

---
+++ PropDeals-ios/Supabase.swift
@@ -0,0 +1,17 @@
+//
+//  Supabase.swift
+//  PropDeals-ios
+//
+//  Created by Armaan Gupta on 12/26/25.
+//
+
+import Supabase
+
+
+let SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwemRsZGNkc3dxd2Nsa29vc2dxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4MzY3OTYsImV4cCI6MjA3NzQxMjc5Nn0.K5cisvseYOwW6PTXkNReMJmSQr0UzA5qwb1T7JrRcGE"
+let SUPABASE_URL = "https://rpzdldcdswqwclkoosgq.supabase.co"
+
+let supabase = SupabaseClient(
+	supabaseURL: URL(string: SUPABASE_URL),
+	supabaseKey: SUPABASE_KEY,
+)
